49d10b8406715dba8c7bd9cd7e49029bfaa30aaf
InZeyn <43585311+InZeyn@users.noreply.github.com>
2019-03-28 18:55:01 -0400

    Add Db

---
+++ HtmlHelpersCore2/HtmlHelpersCore2/Migrations/20190328225344_firstMigration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HtmlHelpersCore2.Migrations
+{
+    public partial class firstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Cedula = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Nombre = table.Column<string>(nullable: false),
+                    Apellido = table.Column<string>(nullable: false),
+                    Edad = table.Column<int>(nullable: false),
+                    Telefono = table.Column<int>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    UserGenero = table.Column<int>(nullable: false),
+                    UserEstadoCivil = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Cedula);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ HtmlHelpersCore2/HtmlHelpersCore2/Models/User.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlHelpersCore2.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+
+    public class User
+    {
+        [Required]
+        [Key]
+        public int Cedula { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public string Apellido { get; set; }
+        [Range(15,double.MaxValue,ErrorMessage = "Debe ser mayor de 15 años")]
+        public int Edad { get; set; }
+        [Phone]
+        public int Telefono { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        public Genero UserGenero { get; set; }
+        public EstadoCivil UserEstadoCivil { get; set; }
+    }
+
+    public enum Genero
+    {
+        Masculino,
+        Femenino
+    }
+
+    public enum EstadoCivil
+    {
+        Casado,
+        Soltero,
+        Viduo,
+        UnionLibre
+    }
+}
